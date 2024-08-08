bfb2b8f16a94d7539a7b992ee59229c6a6b486d6
aditya <dubeyaditya0633@gmail.com>
2024-08-08 14:31:07 +0530

    httt

---
+++ Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public static void main(String [] ar){
+        int a=23;
+        int b =34;
+        System.out.println("value of a "+a);
+        System.out.println("value of b "+b);
+        int c ;
+        c=a;
+        a=b; 
+        b=c;
+        System.out.println("new value of a "+a);
+        System.out.println("new value of b "+b);
+        
+
+    }
+
+    
+}
