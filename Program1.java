3b3fdd5ef1f6c12538fa20900360a9879bc47fa0
sensiop <saurabhsaini.in@gmail.com>
2024-08-08 14:53:04 +0530

    saurabh

--- Program1.java
+++ Program1.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 Class Document	{
+	
+	public static int add(int n, int m){
+	return n+m;
+	}
 	public static void main(String [] args)
 	{
-
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter First Number");
+	int n=sc.nextInt();
+	System.out.println("Enter Second Number");
+	int m= sc.nextInt();
+	add(n,m);
 	}
 
 }
