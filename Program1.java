2dd21d402548eead23729023fa1ed92883ad603b
aditya <dubeyaditya0633@gmail.com>
2024-08-08 15:03:57 +0530

    Ankit ko hthao admin se

--- Program1.java
+++ Program1.java
@@ -1,22 +1,22 @@
-import java.util.Scanner;
-abstract class Document	{
+// import java.util.Scanner;
+// abstract class Document	{
 	
-	public static int add(int n, int m){
-	return n+m;
-	}
-	public static void main(String [] args)
-	{
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter First Number");
-	int n=sc.nextInt();
-	System.out.println("Enter Second Number");
-	int m= sc.nextInt();
-	add(n,m);
-	m2();
-	}
+// 	public static int add(int n, int m){
+// 	return n+m;
+// 	}
+// 	public static void main(String [] args)
+// 	{
+// 	Scanner sc = new Scanner(System.in);
+// 	System.out.println("Enter First Number");
+// 	int n=sc.nextInt();
+// 	System.out.println("Enter Second Number");
+// 	int m= sc.nextInt();
+// 	add(n,m);
+// 	m2();
+// 	}
 
-	abstract void m1();
-	public static void m2(){
-		System.out.println("why ankit is admin");
-	}
-}
+// 	abstract void m1();
+// 	public static void m2(){
+// 		System.out.println("why ankit is admin");
+// 	}
+// }
